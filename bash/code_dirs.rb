c7537d5f4488541231274d4a5aa30168451fd497
Gianluigi Di Donato <gianluigi.didonato@gmail.com>
2011-06-03 22:31:26 +0200

    bash alias and code dir customization

--- bash/code_dirs.rb
+++ bash/code_dirs.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby 
 
-Dir["#{File.expand_path("~/code")}/*"].each { |file|
+Dir["#{File.expand_path("~/workspace")}/*"].each { |file|
     if File.directory?(file)
         cmd=File.basename(file)
         puts "alias #{cmd}=\"cd #{file}\"" if `which #{cmd}`.length == 0
